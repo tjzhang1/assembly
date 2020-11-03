8b5a2cd40b1ac3bb6f41e7c79f0bff18aa34d841
tjzhang1 <tylerjzhang@gmail.com>
2020-11-03 06:32:29 +0000

    move around code to reduce stack usage

--- p6/tstSort.c
+++ p6/tstSort.c
@@ -1,13 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+//#define DEBUG 1
+#ifdef DEBUG
+   #define MAXCNT 30
+#else
+   #define MAXCNT 8
+#endif
 
 unsigned long myGap(void *p0, void *p1)
 {
    size_t a0=(size_t)p0, a1=(size_t)p1;
    return((a0 <= a1) ? a1-a0 : a0-a1);
 }
+#include "word.h"
 
+#ifdef DEBUG
+int LNNUM_=0;
+word_t *lns=NULL;
+void addLn(int ln)
+{
+   word_t *p=lns;
+   for (p=lns; p; p = p->next)
+   {
+      if (p->len == ln) // already seen case, don't add
+         return;
+   }
+   LNNUM_ = ln;
+   p = malloc(sizeof(word_t));
+   assert(p);
+   p->len = ln;
+   p->next = lns;
+   lns = p;
+}
+
+void prntLns(int i)
+{
+   word_t *p=lns;
+   if (!lns)
+   {
+      printf("NO LINES\n");
+      return;
+   }
+   printf("LAST=%d, UNIQUE LINES:\n", LNNUM_);
+   for (p=lns; p; p = p->next)
+      printf("   %u\n", p->len);
+   printf("DONE PRINT LINES\n");
+   while (lns)
+   {
+      p = lns->next;
+      free(lns);
+      lns = p;
+   }
+}
+#endif
 #ifndef WORD_ONLY
 typedef struct items item_t;
 struct items
@@ -46,7 +92,133 @@ unsigned int tstItems(item_t *bp, int which)
 }
 #endif
 
-#include "word.h"
+
+void printAddrs(char *lab, word_t *wb)
+{
+   int n=0;
+   if (!wb)
+      return;
+   printf("%s={%p", lab, wb);
+   for (wb=wb->next; wb && n < 40; wb = wb->next, n++)
+      printf(", %p", wb);
+   printf("}\n");
+}
+void printLens(char *lab, word_t *wb)
+{
+   printAddrs(lab, wb);
+   if (!wb)
+      return;
+   printf("%s={%d", lab, wb->len);
+   for (wb=wb->next; wb; wb = wb->next)
+      printf(", %d", wb->len);
+   printf("}\n");
+}
+
+int tstCycle(unsigned int N, int vrb, word_t *wb)
+/*
+ * Tests wb to see if it contains any cycles
+ * RETURNS: 0 if no error, else non-zero
+ */
+{
+   word_t *p;
+   int i;
+   if (vrb)
+      fprintf(stderr, "MISSCOUNT DETECTION N=%u, base=%p\n", N, wb);
+   if (wb)
+   {
+      if (wb->next == wb)
+      {
+         fprintf(stderr, "   Base ptr %p's next is itself!\n\n", wb);
+         return(1);
+      }
+      for (i=1,p=wb->next; p; p = p->next, i++)
+      {
+         word_t *p0=wb;
+         int i0;
+         if (i >= N)
+         {
+            fprintf(stderr, "   N=%d, but I have more nodes than that!\n", N);
+            fprintf(stderr, "@{%p", wb);
+            for (i=0,p=wb->next; i < N && p; p = p->next, i++)
+               fprintf(stderr, ",%p", p);
+            fprintf(stderr, "}\n\n");
+           return(2);
+         }
+         for (i0=0; i0 < i; i0++)
+         {
+            if (p0->next == p0)
+            {
+               fprintf(stderr, 
+                  "   CYCLE DETECTED: index %u points at itself!\n", i0);
+               return(3);
+            }
+            if (p0 == p)
+            {
+               fprintf(stderr, "   CYCLE DETECTED: index %u points back at"
+                       " index %u!\n", i, i0);
+               return(4);
+            }
+            p0 = p0->next;
+         }
+         while (p0 != p);
+      }
+   }
+   else 
+      i = 0;
+   if (i != N)
+   {
+      fprintf(stderr, "   MISCNT: N=%d, counted=%d\n", N, i);
+      return(5);
+   }
+   if (vrb)
+      fprintf(stderr, "COUNT OK\n");
+   return(0);
+}
+
+int tstAll(unsigned int N, int vrb, word_t *wb)
+{
+   int nerr=0;
+   if (wb == NULL)
+      return(N!=0);
+
+   nerr = tstCycle(N, vrb, wb);
+   if (nerr)         // if data structure messed up
+      return(nerr);   // don't attempt to traverse
+   if (vrb)
+      printf("\nTESTING ORDER N=%d, bp=%p:\n", N, wb);
+   if (N > 1)
+   {
+      word_t *prevW=wb, *wp;
+      int k;
+      for (k=1,wp=wb->next; wp; wp = wp->next, k++)
+      {
+         if (prevW->len > wp->len)
+         {
+            fprintf(stderr, "   node %u out-of-order, value=%d, prev=%d\n", 
+                   k, wp->len, prevW->len);
+            nerr++;
+         }
+         prevW = wp;
+      }
+      if (N != k)
+      {
+         if (N > k)
+            fprintf(stderr, "   SORT LOST %u NODES FROM LIST!\n\n", N-k);
+         else if (k < N)
+            fprintf(stderr, "   SORT ADDED %u NODES!\n\n", k-N);
+         nerr++;
+      }
+   }
+   else if (N == 1)
+       nerr += (!(wb && !wb->next));
+   else /* N == 0 */
+      nerr += (wb != NULL);
+   if (nerr)
+      fprintf(stderr, "FAILED TEST NERR=%d\n", nerr);
+   else if (vrb)
+      fprintf(stderr, "PASS TESTS\n");
+   return(nerr);
+}
 
 int main(int nargs, char **args)
 {
@@ -99,39 +271,22 @@ int main(int nargs, char **args)
             #endif
          #endif
       }
+      if (N <= MAXCNT)
+         printLens("U", wb);
       wp = wb;
       wb = mrgSort(wb, gNxt, gL);
-      if (N > 1)
-      {
-         word_t *prevW=wb;
-         int k;
-         for (k=1,wp=wb->next; wp; wp = wp->next, k++)
-         {
-            if (prevW->len > wp->len)
-            {
-               fprintf(stderr, "node %u out-of-order, value=%d, prev=%d\n", 
-                      k, wp->len, prevW->len);
-               nerr++;
-            }
-            prevW = wp;
-         }
-         if (N != k)
-         {
-            if (N > k)
-               fprintf(stderr, "SORT LOST %u NODES FROM LIST!\n\n", N-k);
-            else if (k < N)
-               fprintf(stderr, "SORT ADDED %u NODES!\n\n", k-N);
-            nerr++;
-         }
-      }
-      else if (N == 1)
-          assert(wb==wp && !wb->next);
-      else /* N == 0 */
-         assert(wb == NULL);
+      #ifdef DEBUG
+         fprintf(stderr, "return=%p\n", wb);
+         prntLns(0);
+      #endif
+      nerr = tstAll(N, 1, wb);
       if (nerr)
       {
          fprintf(stderr, "Stopping testing, bad case N=%u words, NERR=%u\n",
                  N, nerr);
+         fprintf(stderr, "Print of sorted list may hang:\n");
+         if (N>0 && N <= MAXCNT)
+            printLens("S", wb);
          return(nerr);
       }
       fprintf(stderr, "PASS    N=%u case word_t!\n\n", N);
