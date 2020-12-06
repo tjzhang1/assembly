ae47d9ddbd320dc8b1daffb6c6cf1cd453cc8a50
tjzhang1 <tylerjzhang@gmail.com>
2020-12-06 23:05:29 +0000

    preload lb and rb next ptrs

--- final/mrgSort.c
+++ final/mrgSort.c
@@ -19,77 +19,39 @@ void *mrgChains
 #else
 word_t *mrgChainsC
 (
-   word_t *lb,      // address of left base node 
-   word_t *rb,      // address of right base node
-   word_t *endL,    //addr of end node of lb
-   word_t *endR,    //addr of end node of rb
-   word_t **ML      //OUT: points to end of merged list
+//   int nL,          //num nodes remaining in lb
+   word_t *lb,      //address of left base node
+   word_t *rb,      //address of right base node
+   int lbVal,       //val of first node of lb
+   int rbVal        //val of first node of rb
 )
 /*
- * Given two sorted lists started by lb & rb.
- * NOTE: In my implementation, input and output lists NULL terminated.
- * RETURNS: base ptr for merged and sorted list, sets ML to last node
+ * Given two sorted lists started by lb & rb, returns merged and sorted list.
+ * ASSUME: input and output lists NULL terminated
+ * ASSUME: lb will run out of nodes before rb will
+ * RETURNS: base ptr for merged and sorted list
  */
 {
    /*ptrs for base of new list, current node, and next node*/
    word_t *newListHead, *curr;
-   int lbVal, rbVal;   //holds top val of lb and rb
-   int endValL, endValR;   //holds last val of lb and rb
-   //ASSUMES: lb,rb,endL,endR non-NULL due to mrgSortR
-   lbVal = lb->len;
-   rbVal = rb->len;
-   endValL = endL->len;
-   endValR = endR->len;
-   //if greatest val in lb <= smallest val in rb, append rb to lb
-   if(endValL <= rbVal)
-   {
-      *ML = endR;
-      endL->next=rb;
-      return lb;
-   }
-   //if greatest val in rb <= smallest val in lb, append lb to rb
-   else if (endValR <= lbVal)
-   {
-      *ML = endL;
-      endR->next=lb;
-      return rb;
-   }
-   //ASSUME: lb will always run out before rb, saving us extra checks
-   //   This always occors if max lb <= max rb
-   //if max lb > max rb, swap lb and rb to make this assumption true
-   if (endValL > endValR)
-   {
-      word_t *tmp;
-      //last node of merged list is endL now
-      *ML = endL;
-      //swap lb and rb
-      tmp = lb;
-      lb = rb;
-      rb = tmp;
-      //also swap lbLen and rbLen
-      lbVal ^= rbVal;
-      rbVal ^= lbVal;
-      lbVal ^= rbVal;
-   }
-   else
-      //the last node of merged list is endR
-      *ML = endR; 
+   //Ptrs to preload lb->next and rb->next
+   word_t *nextL=lb->next, *nextR=rb->next;
    /*initialize first item in new list by comparing left and right*/
    if(lbVal <= rbVal)
    {
-      newListHead = lb; /*initialize the base ptr for new list*/
-      lb = lb->next;    /*increment the top of the left buffer*/
-      if(!lb)           //if lb empty,
-      {
-         newListHead->next=rb;   //append rb
-         return newListHead;     //return newList
-      }
-      lbVal = lb->len;  //otherwise, update lbVal
+      newListHead = lb; //initialize the base ptr for new list
+      lb = nextL;       //increment the top of the left buffer
+      //Don't need to check if lb empty because that would mean lb has only one
+      //   element < minR. If lb has only one element, then minL=maxL < minR,
+      //   which was already handled in mrgSortRC.
+      nextL = nextL->next;  //begin loading next node into nextL
+      lbVal = lb->len;  //update lbVal
    }
    else
    {
       newListHead = rb; //initialize the base ptr for new list
-      rb = rb->next;    //increment the top of the right buffer
+      rb = nextR;       //increment the top of the right buffer
+      nextR = nextR->next;  //begin loading next node into nextR
       //don't need to check if rb->next is empty, lb empty before rb
       rbVal = rb->len;  //update rbVal
    }
@@ -102,24 +64,27 @@ word_t *mrgChainsC
       {
          curr->next = lb;     //next node is lb
          curr = lb;           //curr=curr->next
-         lb = lb->next;       //increment lb
-         if(!lb)              //if lb empty,
+         lb = nextL;          //increment lb
+         if(!nextL)           //if lb empty,
          {
             curr->next=rb;       //append rb
             return newListHead;
          }
+         nextL = nextL->next; //begin loading next value of nextL
          lbVal = lb->len;  //otherwise, update lbVal
          //continue left: don't need to set the next pointer b/c curr->next
          //already points to the next item in lb
          while(lbVal <= rbVal)
          {
+            //don't need to write curr->next
             curr = lb;
-            lb = lb->next;
-            if(!lb)
+            lb = nextL;
+            if(!nextL)
             {
                curr->next=rb;  //append rb
                return newListHead;
             }
+            nextL = nextL->next;
             lbVal = lb->len;
          }
       }
@@ -128,18 +93,20 @@ word_t *mrgChainsC
       {
          curr->next = rb;     //next node is rb
          curr = rb;           //curr=curr->next
-         rb = rb->next;       //increment rb
+         rb = nextR;          //increment rb
          //don't need to check if rb->next is empty, lb empty before rb
-         rbVal = rb->len;     // update rbVal
+         rbVal = nextR->len;     // update rbVal
+         nextR = nextR->next; //increment nextR
          //continue right: don't need to set the next pointer b/c curr-next
-         //already points to the next item in rb
+         //   already points to the next item in rb
          //can't be <= or else there's a chance rb runs out of nodes first
          while(rbVal < lbVal)  
          {
             curr = rb;
-            rb = rb->next;
+            rb = nextR;
             //don't need to check if rb->next is empty, lb empty before rb
-            rbVal = rb->len;
+            rbVal = nextR->len;
+            nextR = nextR->next;
          }
       }
    } //end while
@@ -207,8 +174,10 @@ word_t *mrgSortRC   /* RETURNS: base of greatest-to-least ->len sorted list */
    //bp is always non-NULL and N>0 thanks to wrapper function
    word_t *lb, *rb;
    word_t *endL, *endR;  //ptrs to last nodes of lb and rb
-   /*initialize lengths of left and right buffers for splitting*/
-   unsigned int leftHalf=(N>>1), rightHalf = N-leftHalf;
+   int minL, minR;       //holds top (min) val of lb and rb
+   int maxL, maxR;       //holds last (max) val of lb and rb
+   //initialize lengths of left and right buffers for splitting
+   unsigned int leftHalf=N>>1, rightHalf = N-leftHalf;
    /*mrgSort on left nodes*/
    lb = mrgSortRC(leftHalf,bp,gNxt,gI,&endL,UB);
    //rb set to next unsorted spot
@@ -217,13 +186,43 @@ word_t *mrgSortRC   /* RETURNS: base of greatest-to-least ->len sorted list */
    rb = mrgSortRC(rightHalf,*UB,gNxt,gI,&endR,UB);
    //UB updated to next unsorted spot
    //endR set to last node in rb
-   
-   //return mergeLists on left and right
-   #ifdef THUMB_CHAINS
-   return mrgChains(lb,rb,gNxt,gI,ML,endL,endR);
-   #else
-   return mrgChainsC(lb,rb,endL,endR,ML);
-   #endif
+   maxL = endL->len;
+   minR = rb->len;
+   minL = lb->len;
+   maxR = endR->len;
+   //if max val in lb <= min val in rb, append rb to lb
+   if(maxL <= minR)
+   {
+      *ML = endR;
+      endL->next=rb;
+      return lb;
+   }
+   //if max val in rb <= smallest val in lb, append lb to rb
+   else if (maxR <= minL)
+   {
+      *ML = endL;
+      endR->next=lb;
+      return rb;
+   }
+   //Make it so lb will always run out of nodes before rb. This always occurs
+   //  if max lb <= max rb. Benefits: simplifying mrgChains loops by reducing
+   //  num of arguments needed to pass in. Since lb runs out of
+   //  nodes before rb, then only need to check how many nodes left in lb.
+   //if max lb > max rb, swap lb and rb to make this assumption true
+   if (maxL > maxR)
+   {
+      //last node of merged list is endL now
+      *ML = endL;
+      //call mrgChains with lb and rb swapped
+      return mrgChainsC(rb,lb,minR,minL);
+   }
+   else
+   {
+      //the last node of merged list is endR
+      *ML = endR;
+      //call mrgChains with lb and rb in standard order
+      return mrgChainsC(lb,rb,minL,minR);
+   }
 }
 #endif 
 
